066611de2757a6f8a1c56e99e4b1aa3d30e30ee3
samueldanso <me.samueldanso@gmail.com>
2024-07-24 23:57:10 +0000

    starting point and readme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
-import Image from 'next/image'
-
 export default function Home() {
-	return <main>ChatPDF</main>
+	return (
+		<main className="flex min-h-screen items-center justify-center">
+			<h1 className="text-4xl font-bold">Hello (ChatPDF)</h1>
+		</main>
+	)
 }
