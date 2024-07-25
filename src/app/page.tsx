b73d83e6979262cc973429091778567db0ea4202
samueldanso <me.samueldanso@gmail.com>
2024-07-25 01:17:47 +0000

    setup clerk auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
-	return (
-		<main className="flex min-h-screen items-center justify-center">
-			<h1 className="text-4xl font-bold">Hello (ChatPDF)</h1>
-		</main>
-	)
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-3xl font-bold">Hello (ChatPDF)</h1>
+    </main>
+  );
 }
